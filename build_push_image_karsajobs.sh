bd566e4eaeab5819f900cc681c19c149c521ab85
Ahmad Munif <ahmad.munif02@gmail.com>
2023-12-04 15:48:46 +0700

    Proyek submission 2

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker build -t ghcr.io/amdmunif/karsajobs:latest .
-export PAT=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
 echo $PAT | docker login ghcr.io --username amdmunif --password-stdin
 # Push docker image to github
 docker push ghcr.io/amdmunif/karsajobs:latest
