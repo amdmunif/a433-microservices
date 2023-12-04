bae8cb12d8ad295fc0feb9a06f41926f6115b0b6
root <root@DESKTOP-N4HAC84>
2023-12-04 15:07:03 +0700

    Proyek submission 2

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build -t ghcr.io/amdmunif/karsajobs:latest .
-export PAT=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
+export PAT=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
 echo $PAT | docker login ghcr.io --username amdmunif --password-stdin
 docker push ghcr.io/amdmunif/karsajobs:latest
