2409380bcd06ec160df8273b1f071f4bab187b95
Ahmad Munif <ahmad.munif02@gmail.com>
2023-12-12 22:40:33 +0700

    Proyek ketiga

---
+++ build_push_image_shipping_service.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build docker image to GitHub Package dengan nama shipping-service
+docker build -t ghcr.io/amdmunif/shipping-service:latest .
+
+# Menggunakan variable untuk menampung password
+export PAT=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
+
+# Menentukan username dan password untuk masuk ke GitHub Package
+echo $PAT | docker login ghcr.io --username amdmunif --password-stdin
+
+# Menentukan username dan password untuk masuk ke GitHub Package dengan nama shipping-service
+docker push ghcr.io/amdmunif/shipping-service:latest
