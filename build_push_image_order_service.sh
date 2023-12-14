4a19ad93fd96039d1a979464e6b4fb5360646873
Ahmad Munif <ahmad.munif02@gmail.com>
2023-12-14 12:39:35 +0700

    Proyek submission 3

--- build_push_image_order_service.sh
+++ build_push_image_order_service.sh
@@ -3,7 +3,7 @@
 # Build docker image to GitHub Package dengan nama order-service
 docker build -t ghcr.io/amdmunif/order-service:latest .
 
-# Menentukan username dan password untuk masuk ke GitHub Package
+# Menentukan username dan password untuk masuk ke GitHub Package 
 echo $PAT | docker login ghcr.io --username amdmunif --password-stdin
 
 # Menentukan username dan password untuk masuk ke GitHub Package dengan nama order-service
